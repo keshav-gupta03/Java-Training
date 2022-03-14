1213ab0db28cfa05c42034710ce44c6a03ff2f04
keshav-gupta03 <94591991+keshav-gupta03@users.noreply.github.com>
2022-03-14 12:39:34 +0530

    Update Question01.java

--- spring_assessment/Question01.java
+++ spring_assessment/Question01.java
@@ -4,5 +4,22 @@ public class Question01 {
     public static void main(String[] args) {
 //        Eager Loading is a design pattern in which data initialization occurs on the spot.
 //        Lazy Loading is a design pattern that we use to defer initialization of an object as long as it's possible.
+        
+        
+//         Lazy and Eager are two types of data loading strategies in ORMs such as hibernate and eclipse Link.  These data loading strategies we used when one entity class is having references to other Entities like Employee and Phone (phone in the employee).
+
+//     Lazy Loading − Associated data loads only when we explicitly call getter or size method.
+
+//     Use Lazy Loading when you are using one-to-many collections.
+//     Use Lazy Loading when you are sure that you are not using related entities.
+//     Eager Loading − Data loading happens at the time of their parent is fetched.
+
+//     Use Eager Loading when the relations are not too much. Thus, Eager Loading is a good practice to reduce further queries on the Server.
+//     Use Eager Loading when you are sure that you will be using related entities with the main entity everywhere.
+
+//     Loading Configuration
+//     Lazy loading can be enabled by using the annotation parameter : fetch = FetchType.LAZY
+//     Eager loading can be enabled by using the annotation parameter : fetch = FetchType.EAGER
+
     }
 }
